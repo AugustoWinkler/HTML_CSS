4608e21b8f782ca7c5bd9dff2c171db346e02eef
AugustoWinkler <111334249+AugustoWinkler@users.noreply.github.com>
2023-09-20 20:49:16 -0300

    new

--- Cards/Infinite Carrosel/script.js
+++ Cards/Infinite Carrosel/script.js
@@ -1,8 +1,18 @@
 const carousel = document.querySelector('.carousel')
 const arrowBtns = document.querySelectorAll('.wrapper i')
 const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+const carouselChildrens = [...carousel.children];
 
 let isDragging = false, startX, startScrollLeft;
+
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth)
+
+carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+    carousel.insertAdjacentHTML("afterbegin", card.outerHTML)
+})
+carouselChildrens.slice(0,cardPerView).forEach(card => {
+    carousel.insertAdjacentHTML("beforeend", card.outerHTML)
+})
 
 arrowBtns.forEach(btn =>{
     btn.addEventListener("click", ()=>{
@@ -31,6 +41,22 @@ const dragStop = () =>{
 
 }
 
+const infiniteScroll = () => {
+    // If the carousel is at the beginning, scroll to the end
+    if(carousel.scrollLeft === 0) {
+        carousel.classList.add("no-transition");
+        carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
+        carousel.classList.remove("no-transition");
+    }
+    // If the carousel is at the end, scroll to the beginning
+    else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
+        carousel.classList.add("no-transition");
+        carousel.scrollLeft = carousel.offsetWidth;
+        carousel.classList.remove("no-transition");
+    }
+}
+
 carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
 document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("scroll", infiniteScroll)
