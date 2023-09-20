9b1226be0526ee06e4f73d4f0a8eb7fc3844d9d2
AugustoWinkler <111334249+AugustoWinkler@users.noreply.github.com>
2023-09-19 22:05:21 -0300

    new

--- Cards/Infinite Carrosel/script.js
+++ Cards/Infinite Carrosel/script.js
@@ -1,18 +1,28 @@
 const carousel = document.querySelector('.carousel')
 
-let isDragging = false;
+let isDragging = false, startX, startScrollLeft;
 
-const dragStart = () => {
+const dragStart = (e) => {
     isDragging = true;
     carousel.classList.add("dragging");
-    
+    startX = e.pageX
+    startScrollLeft = carousel.scrollLeft;
+
 
 }
 
 const dragging = (e) => {
     if(!isDragging) return;
-    carousel.scrollLeft = e.pageX;
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
 
 }
+
+const dragStop = () =>{
+    isDragging = false;
+    carousel.classList.remove("dragging");
+
+}
+
 carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
